67bc8c6480debb14393345563b886f3f2d7dc367
demodogo <macaigm@gmail.com>
2026-06-07 19:12:56 -0400

    chore: initialize spring boot bff

---
+++ backend-bff/src/main/java/cl/duoc/dsy2206/medicalalerts/BackendBffApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.dsy2206.medicalalerts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendBffApplication.class, args);
+	}
+
+}
---
+++ backend-bff/src/test/java/cl/duoc/dsy2206/medicalalerts/BackendBffApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.dsy2206.medicalalerts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
